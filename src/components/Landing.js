32f09a49ec4fbc9576c77203dd2f1e2d15d0f10c
Roqhaye Halimi <roqiahalimi95@gmail.com>
2023-07-08 20:44:34 -0700

    updae hero section in landing component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,27 @@ const Landing = () => {
   return (
     <div className={styles.container}>
       <img src={back} />
-      <h1> از ما چه می خواهید؟</h1>
+      <div className={styles.content}>
+        <h1> از ما چه می خواهید؟</h1>
+        <h5>خانه دلخواه‌تان را به کمک مشاورین متخصص ما پیدا کنید</h5>
+        <div className={styles.form_container}>
+          <div className={styles.form__labels}>
+            <span className={styles.form_container__item}>خرید</span>
+            <span className={styles.form_container__item}>اجاره </span>
+            <span className={styles.form_container__item}> قیمت خانه شما</span>
+          </div>
+          <div className={styles.form__content}>
+            <p>شهر، محله و یا منطقه را وارد کنید </p>
+            <div className={styles.form__button__container}>
+              <input
+                className={styles.form__content__input}
+                placeholder="مثال: نیاوران"
+              ></input>
+              <button className={styles.form__content__button}>جستجو</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -20,25 +20,57 @@ const Navbar1 = () => {
   const bar_two = `${styles.bar} ${styles.bar_two}`;
   const bar_three = `${styles.bar} ${styles.bar_three}`;
   return (
-    <div className={menu_class}>
-      <div className={styles.nav__right}>
-        <div className={styles.nav__menu}>
-          <div className={styles.nav__menu__container} onClick={toggler}>
-            <div className={bar_one}></div>
-            <div className={bar_two}></div>
-            <div className={bar_three}></div>
+    <div className={styles.whiteback}>
+      <div className={menu_class}>
+        <div className={styles.nav__right}>
+          <div className={styles.nav__menu}>
+            <div className={styles.nav__menu__container} onClick={toggler}>
+              <div className={bar_one}></div>
+              <div className={bar_two}></div>
+              <div className={bar_three}></div>
+            </div>
+          </div>
+          <div className={styles.nav__login}>
+            <img src={signin} alt="sign-in" />
+            <Link>ورود</Link>
+            <img src={signup} alt="sign-up" />
+            <Link>ثبتنام</Link>
           </div>
         </div>
-        <div className={styles.nav__login}>
-          <img src={signin} alt="sign-in" />
-          <Link>ورود</Link>
-          <img src={signup} alt="sign-up" />
-          <Link>ثبتنام</Link>
-        </div>
-      </div>
 
-      <div className={styles.nav__left}>
-        <div className={styles.nav__list}>
+        <div className={styles.nav__left}>
+          <div className={styles.nav__list}>
+            <ul>
+              <li>
+                <Link className={styles.nav__list__item} to="/">
+                  پیشنهادهای ویژه
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.nav__list__item} to="/">
+                  آژانس های املاک
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.nav__list__item} to="/">
+                  رهن و اجاره
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.nav__list__item} to="/">
+                  خرید
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.nav__logo__container}>
+            <a className={styles.nav__logo}>
+              <img className={styles.nav__logo__image} src={logo}></img>
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.nav__list2}>
           <ul>
             <li>
               <Link className={styles.nav__list__item} to="/">
@@ -62,36 +94,6 @@ const Navbar1 = () => {
             </li>
           </ul>
         </div>
-        <div className={styles.nav__logo__container}>
-          <a className={styles.nav__logo}>
-            <img className={styles.nav__logo__image} src={logo}></img>
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.nav__list2}>
-        <ul>
-          <li>
-            <Link className={styles.nav__list__item} to="/">
-              پیشنهادهای ویژه
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.nav__list__item} to="/">
-              آژانس های املاک
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.nav__list__item} to="/">
-              رهن و اجاره
-            </Link>
-          </li>
-          <li>
-            <Link className={styles.nav__list__item} to="/">
-              خرید
-            </Link>
-          </li>
-        </ul>
       </div>
     </div>
   );
